35955771121aee9173688b1321e8ea55cd3955ed
Dongyang.Cai <yangguangzaidongji@gmail.com>
2014-04-15 03:43:31 +0800

    Add new source codes.

---
+++ Anagrams.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> anagrams(vector<string> &strs) {
+        map<string, int> tmps;
+		map<string, int>::iterator it;
+		vector<string> vec;
+		for (int i = 0; i < strs.size(); i++) {
+			string s = strs[i];
+			sort(s.begin(), s.end());
+			it = tmps.find(s);
+			if (it == tmps.end()) {
+				tmps[s] = i;
+			} else {
+				if (tmps[s] == -1) {
+					vec.push_back(strs[i]);
+				} else {
+					vec.push_back(strs[tmps[s]]);
+					tmps[s] = -1;
+					vec.push_back(strs[i]);
+				}
+			}
+		}
+		return vec;
+    }
+};
+
+int main(int argc, char *argv[]) {
+	Solution *s = new Solution;
+	
+}
--- Partition_List.cpp
+++ Partition_List.cpp
@@ -10,13 +10,53 @@ using namespace std;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
-        
+		if (head == NULL) {
+			return NULL;
+		}
+		
+	    ListNode *smailer = new ListNode(-1);
+		ListNode *shead = smailer;
+		ListNode *bigger = new ListNode(-1);
+		ListNode *bhead = bigger;
+		
+		while (head != NULL) {
+			if (head->val < x) {
+				shead->next = head;
+				shead = head;
+			} else {
+				bhead->next = head;
+				bhead = head;
+			}
+			
+			head = head->next;
+		}
+		
+		bhead->next = NULL;
+		shead->next = bigger->next;
+		
+		return smailer->next;
     }
 };
 
 int main(int argc, char *argv[]) {
-	
+	ListNode *n1 = new ListNode(1);
+	ListNode *n2 = new ListNode(4);
+	ListNode *n3 = new ListNode(2);
+	n1->next = n2;
+	n2->next = n3;
+	Solution *s = new Solution;
+	ListNode *t = s->partition(n1, 3);
+	while (t != NULL) {
+		cout << t->val << endl;
+		t = t->next;
+	}
 }
