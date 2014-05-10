258627c77cbcd4a9ada9a91233df2ee13f00735a
Dongyang.Cai <yangguangzaidongji@gmail.com>
2014-05-10 15:44:42 +0800

    Add new source files and examnation questions.

---
+++ First_Missing_Positive .cpp
@@ -0,0 +1,22 @@
+/*
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        
+    }
+};
+
+int main(int argc, char *argv[]) {
+	
+}
--- Search_Insert_Position.cpp
+++ Search_Insert_Position.cpp
@@ -5,10 +5,31 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        
+        if (A == NULL || n == 0) {
+			return 0;
+		}
+
+		int low = 0, high = n-1;
+
+		while(low <= high) {
+			int mid = (low+high) / 2;
+			if (A[mid] == target)
+			{
+				return mid;
+			} else if (A[mid] > target) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return low;
     }
 };
 
 int main(int argc, char *argv[]) {
-	
+	int a[] = {1,3,5,6};
+	Solution s;
+	cout << "EHHEHEHEH " << endl;
+	cout << s.searchInsert(a, 4, 2) << endl;
 }
