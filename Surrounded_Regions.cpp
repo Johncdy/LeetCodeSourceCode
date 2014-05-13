e1e84d1afb170bfe1bd88c7f3448a87bcfe51567
Dongyang <yangguangzaidongji@gmail.com>
2014-05-13 12:17:37 +0800

    Add surrounded regions.

--- Surrounded_Regions.cpp
+++ Surrounded_Regions.cpp
@@ -16,10 +16,39 @@ X X X X
 X O X X
 */
 
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
 class Solution {
 public:
-    void solve(vector<vector<char>> &board) {
+    void solve(vector<vector<char> > &board) {
         
+    }
+    
+    void flood_fill(vector<vector<char> > &board, int i, int j) {
+    	if (board[i][j] != 'O')
+    		return;
+    	queue<int> que;
+    	int index = i*board[0].size() + j;
+    	que.push(index);
+    	while (!que.empty()) {
+    		index = que.front();
+    		int row = index/board[0].size();
+    		int col = index%board[0].size();
+    		
+    		if (row > 0 && board[row-1][col] == 'O' )
+    			board[row-1][col] = 'C';
+    		if (row < board[0].size()-1 && board[row+1][col] == 'O')
+    			board[row+1][col] = 'C';
+    		if (col > 0 && board[row][col-1] == 'O')
+    			board[row][col-1] = 'C'
+    		if (col < board[0])
+    		
+    		que.pop();
+    	}
     }
 };
 
