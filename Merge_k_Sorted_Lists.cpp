4568de1c2957ff6eb7f8626fb6bec0d5eaafb830
Dongyang <yangguangzaidongji@gmail.com>
2014-05-14 10:35:56 +0800

    Flood fill algorithm.

---
+++ Merge_k_Sorted_Lists.cpp
@@ -0,0 +1,35 @@
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+struct ListNode {
+	int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        
+    }
+};
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
--- Surrounded_Regions.cpp
+++ Surrounded_Regions.cpp
@@ -25,29 +25,60 @@ using namespace std;
 class Solution {
 public:
     void solve(vector<vector<char> > &board) {
+        if (board.size() <= 1 || board[0].size() <= 1)
+        	return;
+        	
+        for (int i = 0; i < board.size(); i++) {
+        	flood_fill(board, i, 0); 
+        	flood_fill(board, i, board[i].size()-1);
+        }
         
+        for (int i = 0; i < board[0].size(); i++) {
+        	flood_fill(board, 0, i);
+        	flood_fill(board, board.size()-1, i);
+        }
+        
+        for (int i = 0; i < board.size(); i++) {
+        	for (int j = 0; j < board[i].size(); j++) {
+        		if (board[i][j] == 'O')
+        			board[i][j] = 'X';
+        		else if (board[i][j] == 'C')
+        			board[i][j] = 'O';
+        	}
+        }
     }
     
     void flood_fill(vector<vector<char> > &board, int i, int j) {
     	if (board[i][j] != 'O')
     		return;
+    	
     	queue<int> que;
     	int index = i*board[0].size() + j;
     	que.push(index);
     	while (!que.empty()) {
     		index = que.front();
+    		que.pop();
     		int row = index/board[0].size();
     		int col = index%board[0].size();
     		
-    		if (row > 0 && board[row-1][col] == 'O' )
-    			board[row-1][col] = 'C';
-    		if (row < board[0].size()-1 && board[row+1][col] == 'O')
-    			board[row+1][col] = 'C';
-    		if (col > 0 && board[row][col-1] == 'O')
-    			board[row][col-1] = 'C'
-    		if (col < board[0])
+    		board[row][col] = 'C';
     		
-    		que.pop();
+    		if (row > 0 && board[row-1][col] == 'O' ) {
+    			board[row-1][col] = 'C';
+    			que.push((row-1)*board[0].size()+col);
+    		}
+    		if (row < board.size()-1 && board[row+1][col] == 'O'){
+    			board[row+1][col] = 'C';
+    			que.push((row+1)*board[0].size()+col);
+    		}
+    		if (col > 0 && board[row][col-1] == 'O') {
+    			board[row][col-1] = 'C';
+    			que.push(row*board[0].size()+col-1);
+    		}
+    		if (col < board[0].size()-1 && board[row][col+1] == 'O') {
+    			board[row][col+1] = 'C';
+				que.push(row*board[0].size()+col+1);	
+    		}
     	}
     }
 };
@@ -55,5 +86,33 @@ public:
 int main(int argc, char *argv[])
 {
 	Solution s;
+	vector<vector<char> > vec;
+	for (int i = 0; i < 5; i++) {
+		vector<char> v1;	
+		v1.push_back('X');
+		if (i == 1 || i == 3) {
+			v1.push_back('O');
+		} else {
+			v1.push_back('X');
+		}
+		if (i == 1 || i == 2) {
+			v1.push_back('O');
+		} else {
+			v1.push_back('X');
+		}
+		v1.push_back('X');
+		vec.push_back(v1);
+	}
+	
+	for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec[i].size(); j++) {
+        	cout << vec[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+	
+	s.solve(vec);
+	
 	return 0;
 }
